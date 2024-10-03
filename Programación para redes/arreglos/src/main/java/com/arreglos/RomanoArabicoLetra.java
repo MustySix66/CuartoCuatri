3201a3bcc1e2566ac2948b1e40a7330d347abdab
Kevin <antonandrade259@gmail.com>
2024-10-02 21:10:37 -0600

    Avance en el portafolio de evidencias.

--- Programación para redes/arreglos/src/main/java/com/arreglos/RomanoArabicoLetra.java
+++ Programación para redes/arreglos/src/main/java/com/arreglos/RomanoArabicoLetra.java
@@ -9,6 +9,7 @@ public class RomanoArabicoLetra {
     private static final String[] unidades = {"", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve"};
     private static final String[] decenas = {"", "diez", "veinte", "treinta", "cuarenta", "cincuenta", "sesenta", "setenta", "ochenta", "noventa"};
     private static final String[] teens = {"diez", "once", "doce", "trece", "catorce", "quince", "dieciséis", "diecisiete", "dieciocho", "diecinueve"};
+    private static final String[] centenas = {"", "cien", "doscientos", "trescientos", "cuatrocientos", "quinientos", "seiscientos", "setecientos", "ochocientos", "novecientos"};
 
     static {
         romanoANumArabicoMap.put('I', 1);
@@ -41,22 +42,26 @@ public class RomanoArabicoLetra {
             return teens[num - 10];
         } else if (num < 100) {
             return decenas[num / 10] + ((num % 10 != 0) ? " y " + unidades[num % 10] : "");
+        } else if (num < 1000) {
+            return centenas[num / 100] + ((num % 100 != 0) ? " " + numberoAPalabra(num % 100) : "");
+        } else if (num < 4000) { 
+            int miles = num / 1000;
+            return (miles == 1 ? "mil" : unidades[miles] + " mil") + ((num % 1000 != 0) ? " " + numberoAPalabra(num % 1000) : "");
         } else {
             return "fuera de rango";
         }
     }
 
     public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Escribe el numerito en romano");
-            String romano = scanner.nextLine();
-            int arabico = romanoAArabico(romano);
-            String palabra = numberoAPalabra(arabico);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escribe el numerito en romano");
+        String romano = scanner.nextLine().toUpperCase();
+        int arabico = romanoAArabico(romano);
+        String palabra = numberoAPalabra(arabico);
 
-            System.out.println("Número romano: " + romano);
-            System.out.println("Número arábigo: " + arabico);
-            System.out.println("En palabras: " + palabra);
-            scanner.close();
-        }
+        System.out.println("Número romano: " + romano);
+        System.out.println("Número arábigo: " + arabico);
+        System.out.println("En palabras: " + palabra);
+        scanner.close();
     }
-
+}
