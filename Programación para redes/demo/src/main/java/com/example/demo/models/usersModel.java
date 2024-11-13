a6862ee3003e3dc30d24d92f4849540bb524d89c
Six66 <kevinandrade259@gmail.com>
2024-11-13 15:33:40 -0600

    Falta re hacer el script de respaldo

---
+++ Programación para redes/demo/src/main/java/com/example/demo/models/usersModel.java
@@ -0,0 +1,41 @@
+package com.example.demo.models;
+
+
+public class usersModel {
+    
+    private Long id;
+    private String name;
+    private String email;
+    private Integer priority;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Integer getPriority() {
+        return priority;
+    }
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+
+
+
+
+
+}
